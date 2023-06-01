9356b08e11b97acd38872257232ecb39f0ba2dd0
Bilal Mohammad <bmohammad@socle8-4-0.fr.exalog.net>
2023-06-01 12:54:51 +0200

    Fusion des fichier en un seul fichier

--- merge.php
+++ merge.php
@@ -2,19 +2,11 @@
 if (isset($_POST['submit'])) {
     $selectedFiles = $_FILES['selected_files'];
 
-    // Créer un dossier pour les fichiers fusionnés
-    $mergedFolder = '../Fusion-fichier/';
-    mkdir($mergedFolder);
-
     $mergedContent = '';
     foreach ($selectedFiles['tmp_name'] as $key => $tmpName) {
         $file = $selectedFiles['name'][$key];
-        $targetFile = $mergedFolder . $file;
 
-        // Déplacer le fichier téléchargé vers le dossier fusionné
-        move_uploaded_file($tmpName, $targetFile);
-
-        $content = file_get_contents($targetFile);
+        $content = file_get_contents($tmpName);
         $mergedContent .= $content;
     }
 
@@ -22,6 +14,6 @@ if (isset($_POST['submit'])) {
     $mergedFile = 'fichier-fusionne.txt';
     file_put_contents($mergedFile, $mergedContent);
 
-    echo "Les fichiers ont été fusionnés avec succès dans le dossier : $mergedFolder";
+    echo "Les fichiers ont été fusionnés avec succès dans le fichier : $mergedFile";
 }
 ?>
